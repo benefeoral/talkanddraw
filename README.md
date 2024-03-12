82a64eb9e5167cd6b07def5666e98d888cb65469
benefeoral <162716408+benefeoral@users.noreply.github.com>
2024-03-12 08:49:19 +0300

    Kullanım sırasına ve yapıya göre kodlar düzenlendi
    index.html içindeki style'lar silinip style.css'e taşındı.
    style.css'teki css'ler html'deki sırasına göre sıralandı.
    main.js'teki kodlar kullanılma sırasına göre sıralandı.

--- README.md
+++ README.md
@@ -1,63 +1,23 @@
 # Talk And Draw Projesi
+
 ## Proje URL
+
 [talkanddraw.online](https://talkanddraw.online/)
 
 ## Proje Tanımı
 
 Bu proje, kullanıcıların birbirleriyle konuşarak birlikte çizim yapabilmelerini sağlayan bir uygulamadır. Kullanıcılar, uygulamayı kullanarak birbirleriyle konuşabilir ve aynı anda birlikte çizim yapabilirler.
 
-## Proje Nasil Kendi Bilgisayarınızda Çalıştırılır?
-
-Proje, Python programlama dili kullanılarak geliştirilmiştir.
-
-1 - Projenin çalıştırılabilmesi için bilgisayarınızda Python yüklü olmalıdır. Python yüklü değilse, [Python'un resmi web sitesinden](https://www.python.org/downloads/) indirip yükleyebilirsiniz.
-
-2 - Python yüklendikten sonra, projenin bulunduğu dizine terminal üzerinden gidin.
-
-3 - Terminal üzerinden `pip install --user -r requirements.txt` komutunu çalıştırarak projenin içinde kullandığı paketleri yükleyin. Projenin içinde kullanılan paketlerin listesi `requirements.txt` dosyasında bulunmaktadır.
-
-4 - Paketler yüklendikten sonra, terminal üzerinden `python main.py` komutunu çalıştırarak projeyi başlatabilirsiniz. pytohn yazdıktan sonra hangi dosyanın çalıştırılacağını belirtiyoruz. Bizim ana dosyamız "main.py" olduğu için main.py yazdık.
-
 ## Proje Çalışma Mantığı
 
-1 - "/" dizinine GET isteği atıldığında, uygulama kullanıcıya `index.html` dosyasını döner. Bu dosya, kullanıcıya uygulamanın arayüzünü sunar. Bu arayüzde kullanıcılar, konuşma yaparak çizilmesini istediğimiz resmi anlatır.
+1 - Proje ilk olarak index.html dosyası ile başlar.
+
+2 - OpenAI'a istek atarken kullanmak için apikey istenir. Eğerki apikey yok ise apikey'i almak için OpenAI sitesine gidilir ve apikey alınır.
 
 2 - Konuşmaya başlamadan önce "Dinlemeye başla" yazısı olan butona basarak konuşmaya başlayabilirler. Konuşma bittiğinde ve ya "Dinliyor..." yazısı olan butona basarak konuşmayı bitirebilirler.
 
 3 - Konuşmayı tarayıcıların SpeechSynthesisUtterance API'si ile dinleyip sonrasında yazıya çeviririz.
 
-4 - Yazıya çevrilen konuşmayı index.html içindeki "ciz" fonksiyonuna göndeririz. Bu fonksiyon, metni alır ve "/draw" dizinine POST isteği atarak metni ve resim çizilen kısmı(eğer ki ilk defa yapılmış ise boş , yoksa daha önceki fotoğrafların eklendiği son halini ) isteği BODY'sine ekleyip istek atılır.
+4 - Yazıya çevrilen konuşmayı komutlar alt başlığının altında sırasıyla listeleriz. Bu sayede öncesinde söylediklerimizi de görüntüleyebiliriz.
 
-5 - "/draw" dizinine POST isteği atıldığında, BODY'e konulan veriler alınır ve "main.py" içindeki "get_image_from_prompt" fonksiyonuna dinlenin metnin yazıya döndürülmüş hali verilir. Bu fonksiyon, metni alır ve OpenAI istemcisini kullanarak DALL-E modelini çağırıyoruz ve resim üretiyoruz. Üretilen resmi boyutlandırıyoruz ve döndürüyoruz.
-
-6 - Oluşturulan resim index.html'e döner.
-
-7 - "ciz" fonksiyonu, dönen resmi alır ve eğer ilk defa resim çiziliyor ise resmin konulacağı alan temizlenir ve fabric aracılığı ile resim çizilir. Eğer daha önce resim çizilmiş ise, yeni resim eski resmin üzerine çizilir.
-
-## Kelimelerin Anlamı
-
-- **Python**: Genel amaçlı, yüksek seviyeli bir programlama dilidir. Python, nesne yönelimli, yorumlamalı, modüler, birçok platformda çalışabilen ve açık kaynak kodlu bir programlama dilidir.
-
-- **Pip**: Python paketlerini yönetmek için kullanılan bir paket yöneticisidir.
-
-- **Terminal**: Bilgisayar kullanıcılarının komutları girip çalıştırabildiği bir arayüzdür.
-
-- **HTTP**: İnternet üzerinden bilgi alışverişi yapmak için kullanılan bir protokoldür.
-
-- **SUNUCU**: İstemcilere hizmet veren, istemcilerin isteklerini karşılayan bilgisayar.
-
-- **GET**: Sunucudan veri almak için kullanılan bir HTTP metodu. Bu metodu kullanarak sunucudan veri alabiliriz.
-
-- **POST**: Sunucuya veri göndermek için kullanılan bir HTTP metodu. Bu metodu kullanarak sunucuya veri gönderebiliriz.
-
-- **SpeechSynthesisUtterance API**: Tarayıcıların konuşma sentezleme özelliğini kullanmamızı sağlayan bir API.
-
-- **BODY**: HTTP isteğinin içeriğini temsil eden kısım.
-
-- **Fonksiyon**: Programlama dillerinde, belirli bir işi yapmak için kullanılan, adı olan ve parametre alabilen kod bloklarıdır.
-
-- **OpenAI**: Yapay zeka ve dil modelleri geliştiren bir şirket.
-
-- **DALL-E**: OpenAI'nin geliştirdiği, metin verisi alıp resim üreten bir model.
-
-- **Fabric**: Resim çizmek için kullanılan bir kütüphane.
+5 - Yazıyı "ciz" fonksiyonuna göndeririz. Bu fonksiyon, metni alır ve OpenAI istek atarak metni,fotoğrafın boyutunu,kalitesini ve modeli belirtiriz. Daha sonra gelen resmi image'in src'sine koyarız.
--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let apiToken = "";
 // Gerekli HTML elementlerini seçme
-const baslaButonu = document.getElementById("start-btn");
-const komutYazisi = document.getElementById("command");
-const cizmeAnimasyonu = document.getElementById("drawGif");
-const komutListesi = document.getElementById("commandText");
 const anahtariOnaylaButonu = document.getElementById("anahtariOnaylaButonu");
 const apiKeyInput = document.getElementById("apikey");
+const komutYazisi = document.getElementById("command");
+const baslaButonu = document.getElementById("start-btn");
+const cizmeAnimasyonu = document.getElementById("drawGif");
+const komutListesi = document.getElementById("commandText");
+const sonucImage = document.getElementById("sonucImage");
+
 apiKeyInput.value = localStorage.getItem("apikey") || "";
 // Başlangıç değerleri ve değişken tanımları
 let komutDinleniyor = false; // Konuşma tanıma dinleniyor mu?
@@ -15,10 +17,18 @@ sesTanima.lang = "tr-TR"; // Tanınacak dil
 sesTanima.interimResults = false; // Arada sonuçlar gösterilecek mi?
 sesTanima.maxAlternatives = 1; // Alınacak en fazla alternatif sayısı
 
-// Başlatma butonuna tıklama olayını dinleme
-baslaButonu.addEventListener("click", dinlemeyiAcKapa);
+// Boşluk tuşuna basıldığında konuşma tanıma işlemini başlatma/durdurma
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    e.preventDefault();
+    dinlemeyiAcKapa();
+  }
+};
 
 anahtariOnaylaButonu.addEventListener("click", anahtariOnayla);
+
+// Başlatma butonuna tıklama olayını dinleme
+baslaButonu.addEventListener("click", dinlemeyiAcKapa);
 
 function anahtariOnayla() {
   var girilenSifre = apiKeyInput.value;
@@ -31,60 +41,6 @@ function anahtariOnayla() {
   baslaButonu.style.pointerEvents = "inherit";
   document.getElementById("divPass").style.display = "none";
   localStorage.setItem("apikey", apiToken);
-}
-
-// Konuşma sonucuna göre çizim yapma işlevi
-async function ciz(soylenenMetin) {
-  // Çizim işlemi sırasında gösterilecek animasyon
-  cizmeAnimasyonu.style.display = "block";
-  baslaButonu.style.display = "none";
-  try {
-    // Sunucuya POST isteği gönderme
-    debugger;
-    const sonuc = await fetch("https://api.openai.com/v1/images/generations", {
-      headers: {
-        accept: "*/*",
-        "accept-language": "tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "cross-site",
-        Authorization: "Bearer " + apiToken,
-        "Content-Type": "application/json",
-      },
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "POST",
-      body: JSON.stringify({
-        prompt:
-          "WITHOUT ANY DETAILS, DRAW THIS PROMPT WITHOUT A BACKGROUND: " +
-          soylenenMetin,
-        model: "dall-e-3",
-        n: 1,
-        response_format: "b64_json",
-        size: "1024x1024",
-        style: "vivid",
-        quality: "standard",
-      }),
-    });
-    debugger;
-    const gelenSonuc = await sonuc.json();
-    debugger;
-    let b64img = gelenSonuc.data[0].b64_json;
-    //create image from b64
-    const base64Img = "data:image/jpeg;base64," + b64img;
-    const sonucImage = document.getElementById("sonucImage");
-    sonucImage.src = base64Img;
-
-    sonucImage.style.visibility = "visible";
-    // Çizim animasyonu ve başlatma butonunu yeniden gösterme
-    cizmeAnimasyonu.style.display = "none";
-    baslaButonu.style.display = "block";
-  } catch (error) {
-    // Hata durumunda konsola hata mesajı yazdırma
-    console.error("İşlem sırasında bir hata oluştu:", error);
-    cizmeAnimasyonu.style.display = "none";
-    baslaButonu.style.display = "block";
-  }
 }
 
 // Konuşma tanıma işlemini başlatma/durdurma işlevi
@@ -127,10 +83,52 @@ sesTanima.onend = () => {
   komutDinleniyor = false;
 };
 
-// Boşluk tuşuna basıldığında konuşma tanıma işlemini başlatma/durdurma
-document.body.onkeyup = function (e) {
-  if (e.keyCode == 32) {
-    e.preventDefault();
-    dinlemeyiAcKapa();
+// Konuşma sonucuna göre çizim yapma işlevi
+async function ciz(soylenenMetin) {
+  // Çizim işlemi sırasında gösterilecek animasyon
+  cizmeAnimasyonu.style.display = "block";
+  baslaButonu.style.display = "none";
+  try {
+    // Sunucuya POST isteği gönderme
+    const sonuc = await fetch("https://api.openai.com/v1/images/generations", {
+      headers: {
+        accept: "*/*",
+        "accept-language": "tr,en;q=0.9,en-GB;q=0.8,en-US;q=0.7",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        Authorization: "Bearer " + apiToken,
+        "Content-Type": "application/json",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "POST",
+      body: JSON.stringify({
+        prompt:
+          "WITHOUT ANY DETAILS, DRAW THIS PROMPT WITHOUT A BACKGROUND: " +
+          soylenenMetin,
+        model: "dall-e-3",
+        n: 1,
+        response_format: "b64_json",
+        size: "1024x1024",
+        style: "vivid",
+        quality: "standard",
+      }),
+    });
+    const gelenSonuc = await sonuc.json();
+    let gelenImage = gelenSonuc.data[0].b64_json;
+    //create image from b64
+    const base64Img = "data:image/jpeg;base64," + gelenImage;
+    sonucImage.src = base64Img;
+
+    sonucImage.style.visibility = "visible";
+    // Çizim animasyonu ve başlatma butonunu yeniden gösterme
+    cizmeAnimasyonu.style.display = "none";
+    baslaButonu.style.display = "block";
+  } catch (error) {
+    // Hata durumunda konsola hata mesajı yazdırma
+    console.error("İşlem sırasında bir hata oluştu:", error);
+    cizmeAnimasyonu.style.display = "none";
+    baslaButonu.style.display = "block";
   }
-};
+}
